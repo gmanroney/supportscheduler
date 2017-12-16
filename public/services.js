0fd66ec4a94324c1a85457fa0dba2b017f41f9d4
Gerard Moroney <gandc@upraxis.com>
2017-12-16 19:22:39 +0000

    Completed basic integration of angular-bootstrap-calendar from https://github.com/mattlewis92/angular-bootstrap-calendar. It is using hardcoded data in the schedule section; next step is to feed it the data from the schedule datasource for engineers. And remove some of the un-necessary formats we do not need for now.

--- public/services.js
+++ public/services.js
@@ -1,3 +1,23 @@
 swof.service('engSchedService', function() {
   this.empschedid="999";
 });
+
+swof.factory('alert', function($uibModal) {
+
+  function show(action, event) {
+    return $uibModal.open({
+      templateUrl: 'modalContent.html',
+      controller: function() {
+        var vm = this;
+        vm.action = action;
+        vm.event = event;
+      },
+      controllerAs: 'vm'
+    });
+  }
+
+  return {
+    show: show
+  };
+
+});
