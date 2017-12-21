efcd5a8b012bf9ab8e93ee6595610ceffab434a8
Gerard Moroney <gandc@upraxis.com>
2017-12-21 00:27:15 +0000

    test commit

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Set Verbose Logging
 set -x
 
@@ -7,21 +6,21 @@ set -x
 TIMESTAMP=`date +%Y%M%d%H%m`
 
 # Make Backup Directory
-BACKUP_DIR="../backup/${TIMESTAMP}"
-mkdir ${BACKUP_DIR}
+BACKUP_DIR="backup/${TIMESTAMP}"
+mkdir -p ${BACKUP_DIR}
 
 # Backup Application Directories
-cp -rv ../README.md ${BACKUP_DIR}
-cp -rv ../config ${BACKUP_DIR}
-cp -rv ../controller ${BACKUP_DIR}
-cp -rv ../dist ${BACKUP_DIR}
-cp -rv ../functions ${BACKUP_DIR}
-cp -rv ../models ${BACKUP_DIR}
-cp -rv ../package.json ${BACKUP_DIR}
-cp -rv ../public ${BACKUP_DIR}
-cp -rv ../scripts ${BACKUP_DIR}
-cp -rv ../src ${BACKUP_DIR}
-cp -rv ../test ${BACKUP_DIR}
+cp -rv README.md ${BACKUP_DIR}
+cp -rv config ${BACKUP_DIR}
+cp -rv controller ${BACKUP_DIR}
+cp -rv dist ${BACKUP_DIR}
+cp -rv functions ${BACKUP_DIR}
+cp -rv models ${BACKUP_DIR}
+cp -rv package.json ${BACKUP_DIR}
+cp -rv public ${BACKUP_DIR}
+cp -rv scripts ${BACKUP_DIR}
+cp -rv src ${BACKUP_DIR}
+cp -rv test ${BACKUP_DIR}
 
 # Exit Script
 exit 0
