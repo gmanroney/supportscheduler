8b6ecdbb9e00d04f1ca8ea70ace40e488c71af8e
Gerard Moroney <gandc@upraxis.com>
2017-12-21 00:32:43 +0000

    test commit

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,6 +6,9 @@ set -x
 # Configuration Parameters
 TIMESTAMP=`date +%Y%M%d%H%m`
 
+# Log start of backup
+echo "`date`:$0: Backup started" >> logs/backup.log
+
 # Make Backup Directory
 BACKUP_DIR="backup/${TIMESTAMP}"
 mkdir -p ${BACKUP_DIR}
@@ -22,6 +25,9 @@ cp -rv public ${BACKUP_DIR}
 cp -rv scripts ${BACKUP_DIR}
 cp -rv src ${BACKUP_DIR}
 cp -rv test ${BACKUP_DIR}
+
+# Log end of backup
+echo "`date`:$0: Backup finished" >> logs/backup.log
 
 # Exit Script
 exit 0
